084f60f35c854797147da8bebc1c91c342873770
Najmulkhan1 <najmulislam624@gmail.com>
2025-12-10 15:27:29 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const dotenv = require("dotenv"); // dotenv
+dotenv.config();
+// mongodb connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const run = async () => {
+    try{
+        await client.connect();
+
+        await client.db("admin").command({ping: 1});
+        console.log("MongoDB connected");
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
