aaeb1039b7895a40e57e864655f50337fc137145
Najmulkhan1 <najmulislam624@gmail.com>
2025-12-16 15:41:44 +0600

    make some api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv"); // dotenv
 dotenv.config();
+
 // mongodb connection
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -15,6 +16,16 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 const crypto = require('crypto');
 
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./ticket-bari-firebase-adminsdk-fbsvc-.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 function generateTicketId() {
     const timestamp = Date.now();
     const random = crypto.randomBytes(3).toString('hex').toUpperCase();
@@ -23,11 +34,11 @@ function generateTicketId() {
 
 
 // authentication
-const verifyFBToken = async(req, res, next) => {
+const verifyFBToken = async (req, res, next) => {
     const token = req.headers.authorization;
 
-    if(!token){
-        return res.status(401).send({message: 'Unauthorized'})
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized' })
     }
 
     try {
@@ -37,10 +48,10 @@ const verifyFBToken = async(req, res, next) => {
 
         req.decoded = decoded.email
         next()
-        
-        
+
+
     } catch (error) {
-        return res.status(401).send({message: 'Unauthorized'})
+        return res.status(401).send({ message: 'Unauthorized' })
     }
 }
 
@@ -66,9 +77,20 @@ const run = async () => {
         const bookingsCollection = db.collection('bookings')
         const paymentsCollection = db.collection('payments')
 
-        // user related api
 
-        app.get('/users', async(req,res) => {
+        // middleware
+        const verifyVendor = async (req, res, next) => {
+            const email = req.decoded
+            const user = await usersCollection.findOne({ email: email })
+            if (user?.role !== 'vendor') {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+            next()
+        }
+
+        // user related api
+        // admin
+        app.get('/users', verifyFBToken, async (req, res) => {
             const search = req.query.search
             const query = {}
 
@@ -82,7 +104,46 @@ const run = async () => {
             const cursor = usersCollection.find(query).sort({ createdAt: -1 })
             const result = await cursor.toArray()
             res.send(result)
-            
+
+        })
+
+        app.get('/users-profile/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email }
+
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+
+        // User Profile Update API
+        app.patch('/users-update/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const profile = req.body;
+
+
+            const updateDoc = {
+                $set: {
+                    phone: profile.phone,
+                    address: profile.address,
+                    city: profile.city,
+                    bio: profile.bio
+                }
+            };
+
+            const options = { upsert: true };
+
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+
+
+        app.get('/users/:email/role', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ role: user?.role || 'user' })
         })
 
         app.post('/users', async (req, res) => {
@@ -100,7 +161,8 @@ const run = async () => {
             res.send(result)
         })
 
-        app.patch('/users/role/:id', async (req, res) => {
+        // admin
+        app.patch('/users/role/:id', verifyFBToken, async (req, res) => {
             const id = req.params.id
             const updatedUser = req.body
             const query = { _id: new ObjectId(id) }
@@ -108,13 +170,14 @@ const run = async () => {
             res.send(result)
         })
 
-        app.patch('/users/fraud/:id', async (req, res) => {
+        // admin
+        app.patch('/users/fraud/:id', verifyFBToken, async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
 
             const user = await usersCollection.findOne(query)
-            if(!user){
-                return res.status(404).send({message: 'User not found'})
+            if (!user) {
+                return res.status(404).send({ message: 'User not found' })
             }
 
 
@@ -125,18 +188,18 @@ const run = async () => {
             }
 
             const updateUserResult = await usersCollection.updateOne(query, updateRole)
-            
 
-            const deleteTicketsResult = await ticketsCollection.deleteMany({email: user.email}) //or use vendor id 
 
-            res.send({updateUserResult, deleteTicketsResult})
+            const deleteTicketsResult = await ticketsCollection.deleteMany({ email: user.email }) //or use vendor id 
+
+            res.send({ updateUserResult, deleteTicketsResult })
         })
 
 
         // ticket related api
 
         // admin show all tickets
-        app.get('/tickets', async (req, res) => {
+        app.get('/tickets', verifyFBToken, async (req, res) => {
 
             const search = req.query.search
             const query = {}
@@ -154,11 +217,12 @@ const run = async () => {
 
         app.get('/all-tickets', async (req, res) => {
 
-            const query = {status: 'approved'}
+            const query = { status: 'approved', quantity: { $gt: 0 } }
 
-            const result = await ticketsCollection.find(query).toArray()
+            const result = await ticketsCollection.find(query).sort({ createdAt: -1 }).toArray()
             res.send(result)
         })
+
 
         app.get('/tickets/:id', async (req, res) => {
             const id = req.params.id
@@ -168,23 +232,25 @@ const run = async () => {
         })
 
 
-        app.post('/tickets', async (req, res) => {
+        // vendor
+        app.post('/tickets', verifyFBToken, verifyVendor, async (req, res) => {
             const ticket = req.body
             ticket.createdAt = new Date()
             ticket.status = 'pending'
             // const email = req.decoded.email  //verify token 
             const email = ticket.email
 
-            const user = await usersCollection.findOne({email: email})
-            if(user?.role === 'fraud'){
-                return res.status(403).send({message: 'You are not allowed to create a ticket'})
+            const user = await usersCollection.findOne({ email: email })
+            if (user?.role === 'fraud') {
+                return res.status(403).send({ message: 'You are not allowed to create a ticket' })
             }
-            
+
             const result = await ticketsCollection.insertOne(ticket)
             res.send(result)
         })
 
-        app.patch('/tickets/status/:id', async (req, res) => {
+        // admin
+        app.patch('/tickets/status/:id', verifyFBToken, async (req, res) => {
             const id = req.params.id
             const updatedTicket = req.body
             const query = { _id: new ObjectId(id) }
@@ -192,21 +258,54 @@ const run = async () => {
             res.send(result)
         })
 
-        app.get('/my-tickets', async (req, res) => {
+        // vendor
+        app.get('/my-tickets', verifyFBToken, verifyVendor, async (req, res) => {
             const email = req.query.email
             const query = { email: email }
             const result = await ticketsCollection.find(query).toArray()
             res.send(result)
         })
 
+        // admin
+        app.patch('/tickets/advertise/:id', async (req, res) => {
+            const id = req.params.id
+            const { isAdvertised } = req.body
+
+            const filter = { _id: new ObjectId(id) }
+
+            const updateDoc = {
+                $set: {
+                    isAdvertised: isAdvertised
+                }
+            }
+
+            const result = await ticketsCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+        // home page a advertised tickets show 
+
+        app.get('/tickets-advertise', async (req, res) => {
+            const query = { isAdvertised: true }
+            const result = await ticketsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+
         // booking related api
-        app.get('/my-bookings', async (req, res) => {
+        app.get('/my-bookings', verifyFBToken, async (req, res) => {
             const email = req.query.email
+
+            if (req.decoded !== email) {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
 
 
             const pipeLine = [
                 { $match: { email } },
-                
+
                 { $addFields: { ticketId: { $toObjectId: '$ticketId' } } },
                 {
                     $lookup: {
@@ -217,8 +316,9 @@ const run = async () => {
                     }
                 },
                 { $unwind: { path: '$ticket', preserveNullAndEmptyArrays: true } },
+                { $sort: { createdAt: -1 } }
 
-                
+
             ]
 
             const result = await bookingsCollection.aggregate(pipeLine).toArray()
@@ -226,10 +326,10 @@ const run = async () => {
         })
 
 
-        
 
 
-        app.post('/bookings', async (req, res) => {
+
+        app.post('/bookings', verifyFBToken, async (req, res) => {
             const booking = req.body
             booking.createdAt = new Date()
             // const ticketId = booking.ticketId
@@ -262,7 +362,7 @@ const run = async () => {
             res.send({ message: 'Booking successful', result })
         })
 
-        app.patch('/bookings/:id', async (req, res) => {
+        app.patch('/bookings/:id', verifyFBToken, async (req, res) => {
             const id = req.params.id
             const updatedBooking = req.body
             const query = { _id: new ObjectId(id) }
@@ -270,7 +370,9 @@ const run = async () => {
             res.send(result)
         })
 
-        app.get('/vendor/bookings-request', async (req, res) => {
+
+        // vendor
+        app.get('/vendor/bookings-request', verifyFBToken, verifyVendor, async (req, res) => {
             const vendorEmail = req.query.vendorEmail
 
             const pipeLine = [
@@ -293,7 +395,7 @@ const run = async () => {
 
 
         // payment related api
-        app.post('/create-checkout-session', async (req, res) => {
+        app.post('/create-checkout-session', verifyFBToken, async (req, res) => {
             const paymentInfo = req.body
             const session = await stripe.checkout.sessions.create({
                 line_items: [
@@ -326,10 +428,13 @@ const run = async () => {
         });
 
 
-        app.patch('/payment-success', async (req, res) => {
+        app.patch('/payment-success', verifyFBToken, async (req, res) => {
             try {
                 const session_id = req.query.session_id;
                 const session = await stripe.checkout.sessions.retrieve(session_id);
+
+                console.log(session);
+
 
 
                 const trakingId = generateTicketId();
@@ -415,14 +520,85 @@ const run = async () => {
 
         // payment transaction api
 
-        app.get('/payment-transaction', async (req, res) => {
+        app.get('/payment-transaction', verifyFBToken, async (req, res) => {
             const email = req.query.email;
-            console.log(email);
+
+            if (req.decoded !== email) {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
 
             const query = { customer_email: email };
             const result = await paymentsCollection.find(query).sort({ paidAt: -1 }).toArray();
             res.send(result);
         });
+
+
+        //=========================================
+
+        app.get('/vendor/revenue-stats', async (req, res) => {
+            const email = req.query.email;
+
+            const query = { vendorEmail: email };
+            const ticketQuery = { email: email };
+
+            try {
+
+                const bookingStats = await bookingsCollection.aggregate([
+                    { $match: { status: 'paid', ...query } },
+                    {
+                        $group: {
+                            _id: { $dateToString: { format: "%Y-%m-%d", date: { $toDate: "$bookingDate" } } },
+                            revenue: { $sum: "$totalPrice" },
+                            sold: { $sum: "$quantity" }
+                        }
+                    },
+                    { $sort: { _id: 1 } }
+                ]).toArray();
+
+
+                const ticketStats = await ticketsCollection.aggregate([
+                    {
+                        $match: ticketQuery
+                    },
+                    {
+                        $group: {
+                            _id: { $dateToString: { format: "%Y-%m-%d", date: { $toDate: "$createdAt" } } },
+                            added: { $sum: 1 }
+                        }
+                    },
+                    { $sort: { _id: 1 } }
+                ]).toArray();
+
+
+                const allDates = [
+                    ...new Set([
+                        ...bookingStats.map(item => item._id),
+                        ...ticketStats.map(item => item._id)
+                    ])
+                ].sort();
+
+                const finalData = allDates.map(dateStr => {
+                    const foundBooking = bookingStats.find(b => b._id === dateStr);
+                    const foundTicket = ticketStats.find(t => t._id === dateStr);
+                    const date = new Date(dateStr);
+                    const dayMonth = date.toLocaleDateString('default', { day: 'numeric', month: 'short' });
+
+                    return {
+                        name: dayMonth,
+                        revenue: foundBooking ? foundBooking.revenue : 0,
+                        sold: foundBooking ? foundBooking.sold : 0,
+                        added: foundTicket ? foundTicket.added : 0
+                    };
+                });
+
+                res.send(finalData);
+
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({ message: 'Error fetching stats' });
+            }
+        });
+
 
 
 
